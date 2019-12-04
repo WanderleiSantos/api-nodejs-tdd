2f9e90f79dbcb3f5a5e8cf19a1bcac235c42f78b
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2019-12-03 22:52:27 -0300

    Teste inserir conta

--- __tests__/routes/user.test.js
+++ __tests__/routes/user.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+const mail = `${Date.now()}@gmail.com`;
+
 test('Deve listar todos os usuários', () => {
   return request(app)
     .get('/users')
@@ -11,7 +13,6 @@ test('Deve listar todos os usuários', () => {
 });
 
 test('Deve inserir um usuário', () => {
-  const mail = `${Date.now()}@gmail.com`;
   return request(app)
     .post('/users')
     .send({
@@ -26,7 +27,6 @@ test('Deve inserir um usuário', () => {
 });
 
 test('Não deve inserir usuário sem Nome', () => {
-  const mail = `${Date.now()}@gmail.com`;
   return request(app)
     .post('/users')
     .send({
@@ -35,5 +35,39 @@ test('Não deve inserir usuário sem Nome', () => {
     })
     .then(res => {
       expect(res.status).toBe(400);
+    });
+});
+
+test('Não deve inserir usuário sem email', async () => {
+  const result = await request(app)
+    .post('/users')
+    .send({ name: 'Wanderlei', password: '123456' });
+
+  expect(result.status).toBe(400);
+  expect(result.body.error).toBe('Email obrigatorio');
+});
+
+// test('Não deve inserir usuário sem senha', () => {
+//   request(app)
+//     .post('/users')
+//     .send({ name: 'Wanderlei', mail: 'w@w.com' })
+//     .then(res => {
+//       expect(res.status).toBe(400);
+//       expect(res.body.error).toBe('Senha obrigatorio');
+//       done();
+//     });
+// });
+
+test('Não deve inserir usuário com email cadastrado', () => {
+  return request(app)
+    .post('/users')
+    .send({
+      name: 'Wanderlei',
+      mail,
+      password: '123456',
+    })
+    .then(res => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Email já cadastrado');
     });
 });
--- src/services/user.js
+++ src/services/user.js
@@ -1,12 +1,29 @@
 module.exports = app => {
-  const findAll = () => {
-    return app.db('users').select();
+  const findAll = (filter = {}) => {
+    return app
+      .db('users')
+      .where(filter)
+      .select();
   };
 
-  const save = user => {
+  const save = async user => {
     if (!user.name) {
       return { error: 'Nome obrigatório' };
     }
+
+    if (!user.mail) {
+      return { error: 'Email obrigatorio' };
+    }
+
+    if (!user.password) {
+      return { error: 'Senha obrigatorio' };
+    }
+
+    const userDb = await findAll({ mail: user.mail });
+    if (userDb && userDb.length > 0) {
+      return { error: 'Email já cadastrado' };
+    }
+
     return app.db('users').insert(user, '*');
   };
 
