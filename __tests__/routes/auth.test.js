6f9c998e7e6768157c36482b22bf633100ad11e6
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2019-12-11 22:11:58 -0300

    Teste senha incorreta

--- __tests__/routes/auth.test.js
+++ __tests__/routes/auth.test.js
@@ -15,3 +15,28 @@ test('Deve receber token ao logar', () => {
       expect(res.body).toHaveProperty('token');
     });
 });
+
+test('Não deve autenticar usuário com senha errada', () => {
+  const mail = `${Date.now()}@gmail.com`;
+  return app.services.user
+    .save({ name: 'Wanderlei', mail, password: '123456' })
+    .then(() =>
+      request(app)
+        .post('/auth/signin')
+        .send({ mail, password: '1234567' })
+    )
+    .then(res => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha errados');
+    });
+});
+
+test('Não deve autenticar usuário não cadastrado', () => {
+  return request(app)
+    .post('/auth/signin')
+    .send({ mail: 'dsadas@dsadas.com', password: '1234567' })
+    .then(res => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha errados');
+    });
+});
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jwt-simple');
 const bcrypt = require('bcrypt-nodejs');
+const ValidationError = require('../errors/ValidationError');
 
 const secret = '45dsa546d5as';
 
@@ -8,6 +9,7 @@ module.exports = app => {
     app.services.user
       .findOne({ mail: req.body.mail })
       .then(user => {
+        if (!user) throw new ValidationError('Usuário ou senha errados');
         if (bcrypt.compareSync(req.body.password, user.password)) {
           const payload = {
             id: user.id,
@@ -17,7 +19,7 @@ module.exports = app => {
 
           const token = jwt.encode(payload, secret);
           res.status(200).json({ token });
-        }
+        } else throw new ValidationError('Usuário ou senha errados');
       })
       .catch(err => next(err));
   };
